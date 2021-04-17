1d7e843823012a437ba4a8c5b979ea0cb3a82130
Tristan Edwards <52096006+kanrinsha@users.noreply.github.com>
2021-04-17 05:29:33 -0700

    fixed stackoverflow

--- AmazonScalper/Program.cs
+++ AmazonScalper/Program.cs
@@ -1,7 +1,8 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Configuration;
+using System.Threading;
 
 namespace AmazonScalper
 {
@@ -38,54 +39,58 @@ namespace AmazonScalper
         private void AttemptBuy()
         {
             driver.Url = BuyPage;
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
 
-            //main item checking/refresh loop
-            try
+            while (true)
             {
-                driver.FindElement(By.CssSelector("button.style__button__1MgdH")).Click();
-            }
-            catch
-            {
-                Console.WriteLine("Refreshing...");
-                AttemptBuy();
-                return;
-            }
-
-            driver.Url = "https://www.amazon.ca/gp/cart/view.html?ref_=nav_cart";
-            //check to see if the item is in our cart... sometimes the bot can go too fast and/or the item can get snagged faster
-            try
-            {
-                driver.FindElement(By.CssSelector("#sc-buy-box-ptc-button > span:nth-child(1) > input:nth-child(1)")).Click();
-            }
-            catch
-            {
-                Console.WriteLine("Nothing in cart... Retrying...");
-                AttemptBuy();
-                return;
-            }
-
-            //check to see if amazon logged us out
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
-            try
-            {
-                if(driver.FindElement(By.CssSelector("#ap_password")).Displayed)
+                //main item checking/refresh loop
+                try
                 {
-                    driver.FindElement(By.CssSelector("#ap_password")).SendKeys(Password);
-                    driver.FindElement(By.CssSelector("#signInSubmit")).Click();
+                    driver.FindElement(By.CssSelector("button.style__button__1MgdH")).Click();
+
+                    driver.Url = "https://www.amazon.ca/gp/cart/view.html?ref_=nav_cart";
+                    //check to see if the item is in our cart... sometimes the bot can go too fast and/or the item can get snagged faster
+                    try
+                    {
+                        driver.FindElement(By.CssSelector("#sc-buy-box-ptc-button > span:nth-child(1) > input:nth-child(1)")).Click();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Nothing in cart... Retrying...");
+                        AttemptBuy();
+                        break;
+                    }
+
+                    //check to see if amazon logged us out
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+                    try
+                    {
+                        if (driver.FindElement(By.CssSelector("#ap_password")).Displayed)
+                        {
+                            driver.FindElement(By.CssSelector("#ap_password")).SendKeys(Password);
+                            driver.FindElement(By.CssSelector("#signInSubmit")).Click();
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("No login needed...");
+                    }
+
+                    //finalize order
+                    driver.FindElement(By.CssSelector(".place-your-order-button")).Click();
+
+                    Console.WriteLine("Snatched!!!");
+
+                    Environment.Exit(0);
                 }
+                catch(NoSuchElementException)
+                {
+                    Console.WriteLine("Refreshing...");
+                    driver.Url = BuyPage;
+                }
+
+                Thread.Sleep(1000);
             }
-            catch
-            {
-                Console.WriteLine("No login needed...");
-            }
-
-            //finalize order
-            driver.FindElement(By.CssSelector(".place-your-order-button")).Click();
-
-            Console.WriteLine("Snatched!!!");
-
-            Environment.Exit(0);
         }
     }
 }
